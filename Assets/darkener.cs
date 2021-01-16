f6f2f5468e1123c9f074d7f2d3fb85ea9ceb00a6
RIBOUT <pifi77@hotmail.fr>
2021-01-16 19:32:30 +0100

    [Discussion] UI done

---
+++ Assets/darkener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class darkener : MonoBehaviour
+{
+    public bool active = true;
+
+    public Animator _animator;
+
+    public void Activate()
+    {
+        if (active)
+            return;
+
+        _animator.SetBool("Active", true);
+
+        active = true;
+    }
+
+    public void Deactivate()
+    {
+        if (!active)
+            return;
+
+        _animator.SetBool("Active", false);
+
+        active = false;
+    }
+}
